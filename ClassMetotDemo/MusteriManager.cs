a042cf7ada4e31661cbdd359d00d24de788ddcc2
Ahmet Mert ÇALIŞ <calisahmetmert@gmail.com>
2021-01-23 18:49:12 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + "  " + musteri.Soyad + "  "  +  musteri.HesapNo + "  Hesap  numarası ile eklendi"  );
+        }
+
+        public void BakiyeEkle(Musteri musteri, int bakiye)
+        {
+            Console.WriteLine(musteri.HesapNo + " 'lu hesap numarasına" +"  "+ musteri.Ad + " " + musteri.Soyad  +  " adına "  +bakiye +  "  TL eklendi "  );
+            musteri.Bakiye += bakiye;
+        }
+
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + "  " + musteri.Soyad + "  " + musteri.HesapNo + "  Hesap  numarası ile silindi");
+        }
+
+        public void Listele(Musteri[] musteriliste)
+        {
+            foreach (var musteri in musteriliste)
+            {
+                Console.WriteLine("Musteri Id:" +musteri.Id);
+                Console.WriteLine("Musteri Adı:" +musteri.Ad);
+                Console.WriteLine("Musteri Soyadı:" +musteri.Soyad);
+                Console.WriteLine("Musteri Hesap No:" +musteri.HesapNo);
+                Console.WriteLine("Musteri Bakiye:" +musteri.Bakiye);
+                Console.WriteLine("-----------");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+
+            musteri1.Ad = "Mert";
+            musteri1.Id = "1";
+            musteri1.Soyad = "Demir";
+            musteri1.Bakiye = 0;
+            musteri1.HesapNo = 12345;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Ali";
+            musteri2.Id = "2";
+            musteri2.Soyad = "Saydam";
+            musteri2.Bakiye = 10000;
+            musteri2.HesapNo = 12343;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "Ayşe";
+            musteri3.Id = "3";
+            musteri3.Soyad = "Caliskan";
+            musteri3.Bakiye = 999;
+            musteri3.HesapNo = 12342;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+            musteriManager.Add(musteri3);
+
+            Console.WriteLine("----------------------------");
+
+            musteriManager.Delete(musteri2);
+
+            Console.WriteLine("----------------------------");
+
+            musteriManager.BakiyeEkle(musteri1, 993);
+            Console.WriteLine(musteri1.Bakiye);
+
+            musteriManager.Listele(musteriler);
+
+            Console.WriteLine("----------------------------");
+        }
+    }
+}
